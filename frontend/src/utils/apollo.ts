9b368dfd53a4bc40b023aafb0afb7507a98fba2c
VI7V <officialvishavghai@gmail.com>
2026-04-13 11:06:21 +0530

    Updated apollo

--- frontend/src/utils/apollo.ts
+++ frontend/src/utils/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink, gql } from '@apollo/client
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_URL || 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:4000/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
