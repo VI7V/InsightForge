0487792807a87ec518710279f8a91cc58f5bac9b
VI7V <officialvishavghai@gmail.com>
2026-04-13 10:19:37 +0530

    Updated frontend vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: 'public',
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      input: 'public/index.html'
+    }
   },
   server: {
     port: 3000
