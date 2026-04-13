799780a4b7000e52725c7a5a81022e4a269f122c
VI7V <officialvishavghai@gmail.com>
2026-04-13 10:23:38 +0530

    Updated again vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: resolve(__dirname, '.'),
-  publicDir: resolve(__dirname, 'public'),
   build: {
-    outDir: resolve(__dirname, 'build'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html')
-    }
+    outDir: 'build'
   },
   server: {
     port: 3000
